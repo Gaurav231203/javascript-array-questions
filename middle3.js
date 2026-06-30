4853a54badfaec6fbf785d375469d21a00258dbb
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-01 00:15:53 +0530

    Added new files

---
+++ middle3.js
@@ -0,0 +1,5 @@
+//extract  only  midle 3 elements from this array ;
+
+let item= [1,2,3,4,5,6];
+let newarr = item.slice(2,5);
+console.log(newarr)
---
+++ sortandreverse.js
@@ -0,0 +1,4 @@
+//sort alphabaticly and reverse this array
+let name=["rahul","aman","gaurav","sumit","dalla","zora"];
+name.sort().reverse();
+console.log(name);
